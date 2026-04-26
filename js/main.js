375d9b3e8e985784777deaac00073df2476379d0
milo-silversea <milo@silversea-analytics.com>
2026-04-27 02:59:05 +0700

    Update portfolio to AI Agent centric

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@
     if ($('.hero .hero-text h2').length == 1) {
         var typed_strings = $('.hero .hero-text .typed-text').text();
         var typed = new Typed('.hero .hero-text h2', {
-            strings: typed_strings.split(', '),
+            strings: typed_strings.split(','),
             typeSpeed: 100,
             backSpeed: 20,
             smartBackspace: false,
